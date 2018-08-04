5dcf8e54162505964a8550052a6340f41ad96dcb
Andrei Mackenzie <andrei@clypd.com>
2018-08-04 17:12:15 -0400

    js-beautify on build

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 build:
+	js-beautify -r -X src/index.js
 	npx webpack
 	go install github.com/andrei-m/jira-graph/graphcmd
--- src/index.js
+++ src/index.js
@@ -84,49 +84,53 @@ function hidePopup() {
 }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      epic: {},
-    };
-  }
-
-  render() {
-    const { error, isLoaded, epic } = this.state;
-    if (error) {
-      return <div>Error: failed to fetch the epic</div>
-    } else if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return <Graph epic={epic} />
-    }
-  }
-
-  componentDidMount() {
-    var pathparts = window.location.pathname.split('/');
-    var epicKey = pathparts[pathparts.length - 1];
-
-    console.log('loading ' + epicKey);
-    fetch("/api/epics/" + epicKey)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            epic: result,
-          });
-          console.log('loaded ' + epicKey);
-        },
-        (error) => {
-          this.setState({
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
             isLoaded: false,
-            error: true,
-          });
+            epic: {},
+        };
+    }
+
+    render() {
+        const {
+            error,
+            isLoaded,
+            epic
+        } = this.state;
+        if (error) {
+            return <div>Error: failed to fetch the epic</div>
+        } else if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return <Graph epic={epic} />
         }
-      )
-  }
+    }
+
+    componentDidMount() {
+        var pathparts = window.location.pathname.split('/');
+        var epicKey = pathparts[pathparts.length - 1];
+
+        console.log('loading ' + epicKey);
+        fetch("/api/epics/" + epicKey)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        epic: result,
+                    });
+                    console.log('loaded ' + epicKey);
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: false,
+                        error: true,
+                    });
+                }
+            )
+    }
 }
 
 class Graph extends React.Component {
@@ -137,115 +141,115 @@ class Graph extends React.Component {
 
     render() {
         return (
-          <div className = "cy" ref = {this.myRef} />
+            <div className = "cy" ref = {this.myRef} />
         );
     }
 
     componentDidMount() {
-      this.renderGraph(this.myRef.current, this.props.epic);
+        this.renderGraph(this.myRef.current, this.props.epic);
     }
 
     renderGraph(root, data) {
-      var issues = data.issues.map(function(elem) {
-          return {
-              data: Object.assign({
-                  id: elem.key
-              }, elem)
-          }
-      });
+        var issues = data.issues.map(function(elem) {
+            return {
+                data: Object.assign({
+                    id: elem.key
+                }, elem)
+            }
+        });
 
-      var issueEdges = [];
-      for (var i = 0; i < issues.length; i++) {
-          var blockingIssue = issues[i].data.id;
-          var blockedIssues = data.graph[blockingIssue];
-          for (var j = 0; j < blockedIssues.length; j++) {
-              var id = blockingIssue + '_blocks_' + blockedIssues[j];
-              issueEdges.push({
-                  data: {
-                      id: id,
-                      source: blockingIssue,
-                      target: blockedIssues[j]
-                  }
-              });
-          }
-      }
+        var issueEdges = [];
+        for (var i = 0; i < issues.length; i++) {
+            var blockingIssue = issues[i].data.id;
+            var blockedIssues = data.graph[blockingIssue];
+            for (var j = 0; j < blockedIssues.length; j++) {
+                var id = blockingIssue + '_blocks_' + blockedIssues[j];
+                issueEdges.push({
+                    data: {
+                        id: id,
+                        source: blockingIssue,
+                        target: blockedIssues[j]
+                    }
+                });
+            }
+        }
 
-      var cy = cytoscape({
-          container: root,
+        var cy = cytoscape({
+            container: root,
 
-          boxSelectionEnabled: false,
-          autounselectify: true,
+            boxSelectionEnabled: false,
+            autounselectify: true,
 
-          layout: {
-              name: 'dagre',
-              directed: true
-          },
+            layout: {
+                name: 'dagre',
+                directed: true
+            },
 
-          style: [{
-                  selector: 'node',
-                  style: {
-                      'content': 'data(id)',
-                      'text-opacity': 0.8,
-                      'color': '#000000',
-                      'font-size': 18,
-                      'font-weight': 'bold',
-                      'background-color': function(ele) {
-                          return statusToRGB(ele.data('status'))
-                      },
-                      'border-width': 1,
-                      'border-color': '#000000'
-                  }
-              },
+            style: [{
+                    selector: 'node',
+                    style: {
+                        'content': 'data(id)',
+                        'text-opacity': 0.8,
+                        'color': '#000000',
+                        'font-size': 18,
+                        'font-weight': 'bold',
+                        'background-color': function(ele) {
+                            return statusToRGB(ele.data('status'))
+                        },
+                        'border-width': 1,
+                        'border-color': '#000000'
+                    }
+                },
 
-              {
-                  selector: 'edge',
-                  style: {
-                      'curve-style': 'bezier',
-                      'width': 4,
-                      'target-arrow-shape': 'triangle',
-                      'line-color': '#9dbaea',
-                      'target-arrow-color': '#9dbaea'
-                  }
-              }
-          ],
+                {
+                    selector: 'edge',
+                    style: {
+                        'curve-style': 'bezier',
+                        'width': 4,
+                        'target-arrow-shape': 'triangle',
+                        'line-color': '#9dbaea',
+                        'target-arrow-color': '#9dbaea'
+                    }
+                }
+            ],
 
-          elements: {
-              nodes: issues,
-              edges: issueEdges,
-          },
-      });
+            elements: {
+                nodes: issues,
+                edges: issueEdges,
+            },
+        });
 
-      cy.on('tap', function(evt) {
-          if (evt.target === cy) {
-              hidePopup();
-          }
-      });
+        cy.on('tap', function(evt) {
+            if (evt.target === cy) {
+                hidePopup();
+            }
+        });
 
-      cy.on('tap', 'node', function(evt) {
-          showPopup(evt.renderedPosition, {
-              key: this.data('id'),
-              summary: this.data('summary'),
-              status: this.data('status'),
-              estimate: this.data('estimate'),
-              assignee: this.data('assignee'),
-              assigneeImageURL: this.data('assigneeImageURL'),
-              type: this.data('type'),
-              typeImageURL: this.data('typeImageURL'),
-              priority: this.data('priority'),
-              priorityImageURL: this.data('priorityImageURL'),
-              labels: this.data('labels'),
-              flagged: this.data('flagged')
-          });
-      });
+        cy.on('tap', 'node', function(evt) {
+            showPopup(evt.renderedPosition, {
+                key: this.data('id'),
+                summary: this.data('summary'),
+                status: this.data('status'),
+                estimate: this.data('estimate'),
+                assignee: this.data('assignee'),
+                assigneeImageURL: this.data('assigneeImageURL'),
+                type: this.data('type'),
+                typeImageURL: this.data('typeImageURL'),
+                priority: this.data('priority'),
+                priorityImageURL: this.data('priorityImageURL'),
+                labels: this.data('labels'),
+                flagged: this.data('flagged')
+            });
+        });
 
-      cy.on('mouseover', 'node', function(evt) {
-          document.body.style.cursor = 'pointer';
-      });
+        cy.on('mouseover', 'node', function(evt) {
+            document.body.style.cursor = 'pointer';
+        });
 
-      cy.on('mouseout', 'node', function() {
-          document.body.style.cursor = 'default';
-      });
+        cy.on('mouseout', 'node', function() {
+            document.body.style.cursor = 'default';
+        });
     }
 }
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
