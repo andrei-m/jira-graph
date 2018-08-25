03c9737b1170b2b6cbf66e72d7a8ddd1f798a14a
Andrei Mackenzie <andrei@clypd.com>
2018-08-25 14:39:47 -0400

    Prepare to create multiple .js distribution files through Webpack

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 build:
-	js-beautify -r -X src/index.js
+	js-beautify -r -X src/*
 	npx webpack
 	go install github.com/andrei-m/jira-graph/graphcmd
--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,10 @@ module.exports = {
       }
     ]
   },
+  entry: {
+    graph: './src/graph.js',
+  },
+  output: {
+    filename: '[name].js',
+  },
 };
