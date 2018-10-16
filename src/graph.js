cad7e5a23d2810505397490fc5170b2ef065dee8
Andrei Mackenzie <andrei@clypd.com>
2018-10-15 22:45:20 -0400

    Include a '% completed' summary row

--- src/graph.js
+++ src/graph.js
@@ -502,7 +502,10 @@ class EpicStats extends React.Component {
         }
 
         if (totalPoints > 0) {
-            rows.push(<tr class="total"><td>Total</td><td className="points">{totalPoints}</td></tr>);
+            rows.push(<tr className="total"><td>Total</td><td className="points">{totalPoints}</td></tr>);
+            const closedPoints = byStatus['Closed'];
+            const closedPercent = Math.round(closedPoints / totalPoints * 100);
+            rows.push(<tr className="total"><td colspan="2">{closedPoints}/{totalPoints} Closed ({closedPercent}%)</td></tr>)
         }
 
         return (
