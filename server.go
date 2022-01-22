7d3846ba9a13eabadf4f407e74af88846472a687
Ben Garfield <bengarfield@gmail.com>
2022-01-22 16:19:46 -0500

    embed assets into binary to simplify deployment
    - this means the resulting binary contains everything
    necessary to run, no more shipping templates and js
    to the server for running things
    - both templates and assets can be served "live" still
    when we want to do local development and iterate on
    non-Go things. a new flag will be introduced to support this

--- server.go
+++ server.go
@@ -1,12 +1,44 @@
 package graph
 
 import (
+	"embed"
+	"fmt"
+	"html/template"
 	"net/http"
 	"net/url"
 	"path"
 
 	"gopkg.in/gin-gonic/gin.v1"
 )
+
+var (
+	//go:embed dist
+	assetsFS embed.FS
+
+	//go:embed templates
+	templateFS embed.FS
+)
+
+func loadTemplates(r *gin.Engine, useLiveFiles bool) {
+	if useLiveFiles {
+		r.LoadHTMLGlob("templates/*")
+		return
+	}
+	t, err := template.ParseFS(templateFS, "templates/*")
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse embedded templates: %v", err))
+	}
+	r.SetHTMLTemplate(t)
+}
+
+func loadAssets(r *gin.Engine, useLiveFiles bool) {
+	if useLiveFiles {
+		// embed includes the target dir in path, accommodate that when we're using live files instead
+		r.StaticFS("/assets/dist", gin.Dir("./dist", false))
+		return
+	}
+	r.StaticFS("/assets", http.FS(assetsFS))
+}
 
 func StartServer(user, pass, jiraHost string, fc FieldConfig) error {
 	jc := jiraClient{
@@ -20,8 +52,8 @@ func StartServer(user, pass, jiraHost string, fc FieldConfig) error {
 	}
 
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./dist")
+	loadTemplates(r, false)
+	loadAssets(r, false)
 
 	r.GET("/api/epics/:key", gc.getEpicGraph)
 	r.GET("/api/issues/:key/related", gc.getRelatedIssues)
