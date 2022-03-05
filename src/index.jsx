2e4ac6c3c0853241e5f885530038ad4744b5c8a2
Andrei Mackenzie <andrei@clypd.com>
2022-03-05 15:15:16 -0500

    Define IssueList in its own file

--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-    getRecentIssues
-} from './recent';
+    IssueList
+} from './IssueList';
 import './index.css';
-
-class IssueList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            issues: [],
-        };
-    }
-
-    render() {
-        if (this.state.issues.length == 0) {
-            return <div>no recent issues </div>;
-        }
-        var issues = [];
-        for (var i = 0; i < this.state.issues.length; i++) {
-            const e = this.state.issues[i];
-            issues.push(<Issue issueKey={e.key} summary={e.summary} />);
-        }
-        return (
-            <div>
-            <h3>Recently viewed issues</h3>
-            <ul>{issues}</ul>
-          </div>
-        );
-    }
-
-    componentDidMount() {
-        const recentIssues = getRecentIssues();
-        if (recentIssues) {
-            this.setState({
-                issues: recentIssues
-            });
-        }
-    }
-}
-
-class Issue extends React.Component {
-    render() {
-        const path = '/issues/' + this.props.issueKey;
-        return (
-            <li>
-        <a href={path}>{this.props.issueKey} - {this.props.summary}</a>
-      </li>
-        );
-    }
-}
 
 ReactDOM.render(<IssueList />, document.getElementById('root'));
