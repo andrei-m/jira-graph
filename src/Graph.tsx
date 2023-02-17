2c95b85bb3ee4864fc588dc004f45b6239693416
Ben Garfield <bengarfield@gmail.com>
2023-02-17 13:26:17 -0500

    Convert Graph to TSX
    - This is _largely_ just about threading some data shapes
    through from what the API is currently returning, and using
    that to infer/declare the implicit prop and state types
    - A couple small tweaks were made for early returns, and to
    handle pre-initialization states even if they won't practically
    be encountered by the affected functions
    - One eslint rule needed to be switched to use the smarter
    typescript-eslint replacement

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -5,6 +5,28 @@ import { pushRecentIssue } from './recent';
 import { colors } from './colors';
 import './graph.css';
 
+interface FullIssue {
+    key: string;
+    type: string;
+    typeImageURL: string;
+    summary: string;
+    status: string;
+    assignee: string;
+    assigneeImageURL: string;
+    initialEstimate: number;
+    estimate: number;
+    priority: string;
+    priorityImageURL: string;
+    labels: string[];
+    flagged: boolean;
+    sprints: { name: string }[];
+    color: keyof typeof colors;
+    epicKey: string;
+    epicName: string;
+}
+
+type IssueGraphType = { issues: FullIssue[]; graph: Record<string, string[]> };
+
 //TODO: these statuses are all implementation-specific and should be made customizable.
 const statuses = {
     Backlog: 'Backlog',
@@ -16,14 +38,14 @@ const statuses = {
     Closed: 'Closed',
 };
 
-const epicAndMilestoneStatuses = {
-    DevelopmentActive: 'Development Active',
-    OnHold: 'On Hold',
-    Backlog: 'Backlog',
-    Resolved: 'Resolved',
-};
+const epicAndMilestoneStatuses = new Map([
+    ['DevelopmentActive', 'Development Active'],
+    ['OnHold', 'On Hold'],
+    ['Backlog', 'Backlog'],
+    ['Resolved', 'Resolved'],
+]);
 
-function categorizeStatus(s) {
+function categorizeStatus(s: string) {
     if (s === statuses.Backlog || s === statuses.ReadyForDev) {
         return statuses.Backlog;
     }
@@ -33,7 +55,7 @@ function categorizeStatus(s) {
     return s;
 }
 
-function statusToRGB(s) {
+function statusToRGB(s: string) {
     const categorized = categorizeStatus(s);
     if (categorized === statuses.Backlog) {
         return '#ffffff';
@@ -50,7 +72,16 @@ function statusToRGB(s) {
     return '#000000';
 }
 
-class Popup extends React.Component {
+interface PopupProps {
+    selectedEpic: {
+        epic: FullIssue;
+        popupPosition: {
+            x: number;
+            y: number;
+        };
+    } | null;
+}
+class Popup extends React.Component<PopupProps> {
     render() {
         if (this.props.selectedEpic == null) {
             return <div className='empty'></div>;
@@ -93,7 +124,7 @@ class Popup extends React.Component {
     }
 }
 
-class PopupSprint extends React.Component {
+class PopupSprint extends React.Component<{ sprints: { name: string }[] }> {
     render() {
         const sprints = this.props.sprints;
         if (sprints.length === 0) {
@@ -103,7 +134,7 @@ class PopupSprint extends React.Component {
     }
 }
 
-class PopupIcon extends React.Component {
+class PopupIcon extends React.Component<{ imageURL: string; alt: string }> {
     render() {
         return (
             <span className='popup'>
@@ -113,7 +144,7 @@ class PopupIcon extends React.Component {
     }
 }
 
-class PopupAssignee extends React.Component {
+class PopupAssignee extends React.Component<{ assignee: string; assigneeImageURL: string }> {
     render() {
         if (this.props.assignee === '') {
             return <span className='popup-avatar' />;
@@ -128,7 +159,7 @@ class PopupAssignee extends React.Component {
     }
 }
 
-class PopupKey extends React.Component {
+class PopupKey extends React.Component<{ epicKey: string }> {
     render() {
         const url = `/api/issues/${this.props.epicKey}/details`;
 
@@ -142,13 +173,13 @@ class PopupKey extends React.Component {
     }
 }
 
-class PopupEstimate extends React.Component {
+class PopupEstimate extends React.Component<{ estimate: number }> {
     render() {
         return <span className='popup-estimate'>{this.props.estimate === 0 ? '-' : this.props.estimate}</span>;
     }
 }
 
-class PopupStatus extends React.Component {
+class PopupStatus extends React.Component<{ status: string }> {
     render() {
         const style = {
             backgroundColor: statusToRGB(this.props.status),
@@ -157,7 +188,7 @@ class PopupStatus extends React.Component {
     }
 }
 
-class PopupLabels extends React.Component {
+class PopupLabels extends React.Component<{ labels: string[] }> {
     render() {
         const labelListItems = this.props.labels.map((label) => <li key={label}>{label}</li>);
         return (
@@ -167,23 +198,31 @@ class PopupLabels extends React.Component {
         );
     }
 }
+interface GraphAppProps {
+    initialEstimate: number;
+    toggleMenu: (show: boolean | undefined) => void;
+    issueKey: string;
+    issueType: string;
+}
+interface GraphAppState {
+    error: boolean;
+    isLoaded: boolean;
+    issueGraph: IssueGraphType;
+    selectedEpics: Map<string, boolean>;
+}
+class GraphApp extends React.Component<GraphAppProps, GraphAppState> {
+    state: GraphAppState = {
+        error: false,
+        isLoaded: false,
+        issueGraph: { issues: [], graph: {} },
+        selectedEpics: new Map(),
+    };
 
-class GraphApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            issueGraph: {},
-            selectedEpics: new Map(),
-        };
-    }
-
-    initSelectedEpics(issueGraph) {
+    initSelectedEpics(issueGraph: IssueGraphType) {
         return new Map(issueGraph.issues.map(({ epicKey }) => [epicKey, true]));
     }
 
-    handleEpicSelection(val) {
+    handleEpicSelection(val: React.ChangeEvent<HTMLInputElement>) {
         const epicKey = val.target.value;
         const selected = val.target.checked;
         this.setState((prevState) => ({
@@ -208,7 +247,7 @@ class GraphApp extends React.Component {
                     <Legend
                         issueGraph={this.state.issueGraph}
                         selectedEpics={this.state.selectedEpics}
-                        handleEpicSelection={(epic) => this.handleEpicSelection(epic)}
+                        handleEpicSelection={this.handleEpicSelection}
                     />
                 </div>
             );
@@ -245,7 +284,12 @@ class GraphApp extends React.Component {
     }
 }
 
-class Menu extends React.Component {
+class Menu extends React.Component<{
+    issueKey: string;
+    issueColor: keyof typeof colors;
+    showMenu: boolean;
+    toggleMenu: (e: boolean | undefined) => void;
+}> {
     render() {
         const labelStyle = {
             backgroundColor: colors[this.props.issueColor],
@@ -264,7 +308,7 @@ class Menu extends React.Component {
                     type='checkbox'
                     id='menu-toggle'
                     checked={this.props.showMenu}
-                    onChange={() => this.props.toggleMenu()}
+                    onChange={() => this.props.toggleMenu(undefined)}
                 />
                 <div className='menu'>
                     Related issues
@@ -276,15 +320,17 @@ class Menu extends React.Component {
     }
 }
 
-class RelatedIssues extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            issues: [],
-        };
-    }
+interface RelatedIssuesState {
+    error: boolean;
+    isLoaded: boolean;
+    issues: FullIssue[];
+}
+class RelatedIssues extends React.Component<{ issueKey: string }, RelatedIssuesState> {
+    state: RelatedIssuesState = {
+        error: false,
+        isLoaded: false,
+        issues: [],
+    };
 
     render() {
         if (this.state.error) {
@@ -297,7 +343,7 @@ class RelatedIssues extends React.Component {
             return <div>none!</div>;
         }
 
-        const statusToEpics = issues.reduce(
+        const statusToEpics = issues.reduce<Record<string, FullIssue[]>>(
             (acc, issue) => ({
                 ...acc,
                 [issue.status]: (acc[issue.status] ?? []).concat(issue),
@@ -310,8 +356,7 @@ class RelatedIssues extends React.Component {
         }
 
         const sections = [];
-        for (const issueStatus in epicAndMilestoneStatuses) {
-            const statusString = epicAndMilestoneStatuses[issueStatus];
+        for (const [, statusString] of epicAndMilestoneStatuses) {
             if (statusToEpics[statusString]) {
                 sections.push(<RelatedIssuesSection issues={statusToEpics[statusString]} header={statusString} />);
             }
@@ -348,7 +393,7 @@ class RelatedIssues extends React.Component {
     }
 }
 
-class RelatedIssuesSection extends React.Component {
+class RelatedIssuesSection extends React.Component<{ header?: string; issues: FullIssue[] }> {
     render() {
         const issues = this.props.issues;
 
@@ -370,9 +415,18 @@ class RelatedIssuesSection extends React.Component {
     }
 }
 
-class Graph extends React.Component {
-    myRef = React.createRef();
-    state = {
+interface GraphProps {
+    issueGraph: IssueGraphType;
+    selectedEpics: Map<string, boolean>;
+    toggleMenu: (show: boolean | undefined) => void;
+}
+interface GraphState {
+    selectedEpic: PopupProps['selectedEpic'];
+    cy: cytoscape.Core | null;
+}
+class Graph extends React.Component<GraphProps, GraphState> {
+    myRef = React.createRef<HTMLDivElement>();
+    state: GraphState = {
         selectedEpic: null,
         cy: null,
     };
@@ -387,17 +441,18 @@ class Graph extends React.Component {
     }
 
     componentDidMount() {
-        this.initCy(this.myRef.current);
+        // TODO: handle possibly-null current ref
+        this.initCy(this.myRef.current!);
     }
 
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate(prevProps: typeof this.props, prevState: typeof this.state) {
         if (prevState.cy === null || this.shouldUpdateGraph(prevProps.selectedEpics, this.props.selectedEpics)) {
             this.renderGraph(this.props.issueGraph, this.props.selectedEpics);
         }
     }
 
-    shouldUpdateGraph(prevSelectedEpics, selectedEpics) {
-        for (let key of selectedEpics.keys()) {
+    shouldUpdateGraph(prevSelectedEpics: Map<string, boolean>, selectedEpics: Map<string, boolean>) {
+        for (const key of selectedEpics.keys()) {
             if (prevSelectedEpics.get(key) !== selectedEpics.get(key)) {
                 return true;
             }
@@ -405,7 +460,7 @@ class Graph extends React.Component {
         return false;
     }
 
-    initCy(root) {
+    initCy(root: HTMLElement) {
         const multipleEpics = this.props.selectedEpics.size > 1;
         const cy = cytoscape({
             container: root,
@@ -447,6 +502,7 @@ class Graph extends React.Component {
                                 return '#000000';
                             }
                             if (ele.data('color') in colors) {
+                                // @ts-expect-error TODO: is it possible to fix ele.data return type?
                                 return colors[ele.data('color')];
                             }
                             return '#000000';
@@ -457,7 +513,7 @@ class Graph extends React.Component {
                                 return 'octagon';
                             }
                             if (labels.indexOf('platform') > -1 || labels.indexOf('gillnet') > -1) {
-                                return 'square';
+                                return 'rectangle';
                             }
                             if (labels.indexOf('ui') > -1) {
                                 return 'ellipse';
@@ -512,7 +568,13 @@ class Graph extends React.Component {
         });
     }
 
-    renderGraph(issueGraph, selectedEpics) {
+    renderGraph(issueGraph: IssueGraphType, selectedEpics: Map<string, boolean>) {
+        // This is always initialized by now in practice, but TypeScript doesn't know that
+        if (this.state.cy === null) {
+            console.log('Graph cytoscape instance was not found, skipping render');
+            return;
+        }
+
         const filteredIssues = issueGraph.issues.filter((issue) => selectedEpics.get(issue.epicKey));
 
         const issueKeys = new Map(filteredIssues.map(({ key }) => [key, true]));
@@ -562,24 +624,33 @@ class Graph extends React.Component {
 }
 
 function RoutedIssueGraph() {
-    let params = useParams();
+    const params = useParams();
     return <IssueGraph issueKey={params.issueKey} />;
 }
 
-class IssueGraph extends React.Component {
-    state = {
+interface IssueGraphState {
+    showMenu: boolean;
+    error: boolean;
+    issue: FullIssue | null;
+    jiraHost: string | null;
+    isLoaded: boolean;
+}
+// TODO: issueKey is not optional and will always be present
+class IssueGraph extends React.Component<{ issueKey?: string }, IssueGraphState> {
+    state: IssueGraphState = {
         showMenu: false,
-        error: null,
+        error: false,
         issue: null,
         jiraHost: null,
         isLoaded: false,
     };
 
     render() {
-        if (this.state.error) {
-            return <div>Error: failed to fetch the issue</div>;
-        } else if (!this.state.isLoaded) {
+        if (!this.state.isLoaded) {
             return <div>Loading...</div>;
+        }
+        if (this.state.error || this.state.issue === null) {
+            return <div>Error: failed to fetch the issue</div>;
         }
         const issue = this.state.issue;
         const issueURL = `https://${this.state.jiraHost}/browse/${issue.key}`;
@@ -599,19 +670,19 @@ class IssueGraph extends React.Component {
                     <a href={issueURL} target='_blank' rel='noreferrer'>
                         {issueLabel}
                     </a>
-                    <PopupAssignee assignee={issue.assignee} assigneeImageURL={this.props.issueAssigneeImageURL} />
+                    <PopupAssignee assignee={issue.assignee} assigneeImageURL={issue.assigneeImageURL} />
                 </h1>
                 <GraphApp
                     issueKey={issue.key}
                     issueType={issue.type}
                     initialEstimate={issue.initialEstimate}
-                    toggleMenu={(show) => this.toggleMenu(show)}
+                    toggleMenu={this.toggleMenu}
                 />
             </div>
         );
     }
 
-    toggleMenu(show) {
+    toggleMenu(show: boolean | undefined) {
         this.setState((prevState) => ({
             ...prevState,
             ...{
@@ -642,7 +713,8 @@ class IssueGraph extends React.Component {
                     },
                 }));
                 const issue = {
-                    key: this.props.issueKey,
+                    // TODO: issueKey is not optional and will always be present
+                    key: this.props.issueKey!,
                     summary: result.issue.summary,
                 };
                 pushRecentIssue(issue);
@@ -653,7 +725,7 @@ class IssueGraph extends React.Component {
                 this.setState((prevState) => ({
                     ...prevState,
                     ...{
-                        isLoaded: false,
+                        isLoaded: true,
                         error: true,
                         issue: null,
                         jiraHost: null,
@@ -663,7 +735,7 @@ class IssueGraph extends React.Component {
     }
 }
 
-class EpicStats extends React.Component {
+class EpicStats extends React.Component<{ initialEstimate: number; issueGraph: IssueGraphType }> {
     render() {
         const byStatus = this.getBreakdownByStatus();
         const initialEstimateRow =
@@ -697,7 +769,7 @@ class EpicStats extends React.Component {
         const closedPointsRow =
             totalPoints > 0 ? (
                 <tr className='total'>
-                    <td colSpan='2'>
+                    <td colSpan={2}>
                         {closedPoints}/{totalPoints} Closed ({Math.round((closedPoints / totalPoints) * 100)}%)
                     </td>
                 </tr>
@@ -708,7 +780,7 @@ class EpicStats extends React.Component {
                 <table>
                     <thead>
                         <tr>
-                            <th colSpan='2'>Point Breakdown</th>
+                            <th colSpan={2}>Point Breakdown</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -724,7 +796,7 @@ class EpicStats extends React.Component {
 
     getBreakdownByStatus() {
         const issueGraph = this.props.issueGraph;
-        return issueGraph.issues.reduce((result, iss) => {
+        return issueGraph.issues.reduce<Record<string, number>>((result, iss) => {
             let status = categorizeStatus(iss.status);
             if (status === statuses.ResolvedOnStaging) {
                 status = statuses.InProgress;
@@ -738,13 +810,17 @@ class EpicStats extends React.Component {
     }
 }
 
-class Legend extends React.Component {
+class Legend extends React.Component<{
+    issueGraph: IssueGraphType;
+    selectedEpics: Map<string, boolean>;
+    handleEpicSelection: React.ChangeEventHandler<HTMLInputElement>;
+}> {
     render() {
         const issues = this.props.issueGraph.issues;
         const selectedEpics = this.props.selectedEpics;
 
         //TODO: flatten the epics once in GraphApp to resolve a prop of epic->color code and state for epic->selected
-        const epicKeyToInfo = issues.reduce(
+        const epicKeyToInfo = issues.reduce<Record<string, Pick<FullIssue, 'color' | 'epicName'>>>(
             (map, iss) => ({
                 ...map,
                 [iss.epicKey]: {
